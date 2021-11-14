dbd306d61ddf4908d7260f92c611edd09a365c4d
Gustavo Garozzo <gustavogarozzo@gmail.com>
2021-11-14 13:26:14 -0300

    update db

--- tasks.sql
+++ tasks.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost
--- Tiempo de generación: 13-11-2021 a las 16:39:20
+-- Tiempo de generación: 14-11-2021 a las 16:25:31
 -- Versión del servidor: 8.0.27
 -- Versión de PHP: 7.4.25
 
@@ -24,33 +24,87 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `folders`
+--
+
+CREATE TABLE `folders` (
+  `id_folder` int NOT NULL,
+  `folder` varchar(30) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `folders`
+--
+
+INSERT INTO `folders` (`id_folder`, `folder`) VALUES
+(3, 'Daily Tasks'),
+(4, 'College'),
+(5, 'Others'),
+(8, 'Work');
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `tasks`
 --
 
 CREATE TABLE `tasks` (
   `id` int NOT NULL,
+  `folder_id` int NOT NULL,
   `tarea` text CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `tasks`
+--
+
+INSERT INTO `tasks` (`id`, `folder_id`, `tarea`) VALUES
+(203, 3, 'Daily'),
+(206, 4, 'College'),
+(207, 5, 'Others');
 
 --
 -- Índices para tablas volcadas
 --
 
 --
+-- Indices de la tabla `folders`
+--
+ALTER TABLE `folders`
+  ADD PRIMARY KEY (`id_folder`);
+
+--
 -- Indices de la tabla `tasks`
 --
 ALTER TABLE `tasks`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`) USING BTREE,
+  ADD KEY `task-folder-fk` (`folder_id`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
 --
+-- AUTO_INCREMENT de la tabla `folders`
+--
+ALTER TABLE `folders`
+  MODIFY `id_folder` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
 -- AUTO_INCREMENT de la tabla `tasks`
 --
 ALTER TABLE `tasks`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=148;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=208;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `tasks`
+--
+ALTER TABLE `tasks`
+  ADD CONSTRAINT `tasks_ibfk_1` FOREIGN KEY (`folder_id`) REFERENCES `folders` (`id_folder`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
